8d2107b00555caa80565203be7ac07f073f34df5
Jon Lauters <jonathan.lauters@gmail.com>
2016-08-15 14:55:37 -0500

    condense coll_info sections. added to matching / scoring section

--- pubmatching.py
+++ pubmatching.py
@@ -22,7 +22,37 @@ def matchPubFields(pbdb, idigbio):
 
   for pb in pbdb:
 
-    # TODO: condense coll_info ??
+    pb['member'] = []
+    pb['formation'] = []
+    pb['localities'] = []
+    pb['collectors'] = []
+
+    print "Parsing coll_info for [" + pb['pid'] + "]"
+    for coll_info in pb['coll_info']:
+      print coll_info 
+
+      # condense state into one state list
+      if coll_info['state'].lower() not in pb['states'] and coll_info['state'] is not "":
+        pb['states'].append( coll_info['state'].lower() )
+
+      # condense member into one member list
+      if coll_info['member'].lower() not in pb['member'] and coll_info['member'] is not "":
+        pb['member'].append( coll_info['member'].lower() )
+      
+      # condense formation into one list
+      if coll_info['formation'].lower() not in pb['formation'] and coll_info['formation'] is not "":
+        pb['formation'].append( coll_info['formation'].lower() )
+
+      # condense locality into one list
+      if coll_info['collection_name'].lower() not in pb['localities'] and coll_info['collection_name'] is not "":
+        pb['localities'].append( coll_info['collection_name'].lower() )
+
+      if coll_info['collection_aka'].lower() not in pb['localities'] and coll_info['collection_aka'] is not "":
+        pb['localities'].append( coll_info['collection_aka'].lower() )
+
+      # condense collectors into one list
+      if coll_info['collectors'].lower() not in pb['collectors'] and coll_info['collectors'] is not "":
+        pb['collectors'].append( coll_info['collectors'].lower() )
 
     for specimen in idigbio:
 
@@ -90,10 +120,36 @@ def matchPubFields(pbdb, idigbio):
           score = score + 1
           matched_on.append("PBDB:pubtitle == dwc:associatedReferences")
 
-      #if "dwc:occurrenceRemarks" in specimen['data']:
+      if "dwc:occurrenceRemarks" in specimen['data']:
         # Taxon Name, Locality, State, County,  Stratigraphic Identifier?
 
-      #  if specimen['data']['dwc:occurrenceRemarks'].lower().find( pb['.....'].lower() ) >= 0:
+        for locality in pb['localities']:
+          if specimen['data']['dwc:occurrenceRemarks'].lower().find( locality.lower() ) >= 0:
+            print "== == == PB Locality found in OccRem == == =="
+
+            score = score + 1
+            matched_on.append("PBDB:locality == dwc:occurrenceRemarks")
+
+        for state in pb['states']:
+          if specimen['data']['dwc:occurrenceRemarks'].lower().find( state ) >= 0:
+            print "== == == PB State found in OccRem == == =="
+
+            score = score + 1
+            matched_on.append("PBDB:states == dwc:occurrenceRemarks") 
+
+        for member in pb['member']:
+          if specimen['data']['dwc:occurrenceRemarks'].lower().find( member ) >= 0:
+            print "== == == PB member foun in OccRem == == ==" 
+          
+            score = score + 1
+            matched_on.append("PBDB:member == dwc:occurrenceRemarks")
+
+        for form in pb['formation']:
+          if specimen['data']['dwc:occurrenceRemarks'].lower().find( form ) >= 0:
+            print "== == == PB formation found in OccRem == == =="
+ 
+            score = score + 1
+            matched_on.append("PBDB:formation == dwc:occurrenceRemarks")
 
       if "dwc:identificationReferences" in specimen['data']:
         # People,
@@ -124,7 +180,7 @@ def matchPubFields(pbdb, idigbio):
 
       if "dwc:formation" in specimen['data']:
       
-        if pb['title'].lower().find( specimen['data']['dwc:formation'].lower() ) > 0:
+        if pb['title'].lower().find( specimen['data']['dwc:formation'].lower() ) >= 0:
           print "== == == Formation found in Title == == =="
 
           score = score + 1
@@ -136,11 +192,26 @@ def matchPubFields(pbdb, idigbio):
       #if "dwc:dateIdentified" in specimen:
         # Occurence Date
 
-      #if "dwc:scientificName" in specimen:
+      if "dwc:scientificName" in specimen['data']:
         # should match classification path 
+      
+        for path in pb['classification_path']:
+          if path.lower().find( specimen['data']['dwc:scientificName'].lower() ) >= 0:
+            print "== == == Classification Path matched scientificName == == =="
 
-      #if "dwc:order" in specimen:
+            score = score + 1
+            matched_on.append("PBDB:Classification Path == dwc:scientificName")
+
+      # TODO: dedupe classification path
+      #if "dwc:order" in specimen['data']:
         # should match classification path, sci_names
+
+      #  for path in pb['classification_path']:
+      #    if path.lower().find( specimen['data']['dwc:order'].lower() ) >= 0:
+      #      print "== == == Classification Path matched order == == =="
+
+      #      score = score + 1
+      #      matched_on.append("PBDB:Classification Path == dwc:order")
 
       if "dwc:stateProvince" in specimen['data']:
         # should match collection info
@@ -161,11 +232,18 @@ def matchPubFields(pbdb, idigbio):
 
           score = score + 1
           matched_on.append("PBDB:title == dwc:locality")
+
+        for locality in pb['localities']:
+          if locality.lower().find( specimen['data']['dwc:locality'].lower() ) >= 0:
+            print "== == == Locality matched dwc:locality == == =="
+
+            score = score + 1
+            matched_on.append("PBDB:localities == dwc:locality")
+
       
       if score > 1:
         matches.append({"pbdb_id": pb['pid'], "idig_id": specimen['uuid'], "score": score, "matched_on": '[%s]' % ', '.join(map(str, matched_on)) })
 
-    
+  # Sort matches by descending score  
   sorted_matches = sorted(matches, key=lambda match: match['score'], reverse=True)
-
   return sorted_matches
